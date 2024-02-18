95b43ff2a2e9c23ec7c7b23bab52a3d2f9e4e9b9
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-02-18 11:46:57 +0200

    add slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
-    contacts: {
-      items: [],
-    },
-    filters: {
-      name: "",
-    },
+    contacts: contactsReducer,
+    filters: filtersReducer,
   },
 });
